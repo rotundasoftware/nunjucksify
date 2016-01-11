0ef2ebf8f15b7fc8a846be008a5a6f989bfea512
Ivan Nikolić <niksy5@gmail.com>
2016-01-12 00:37:16 +0100

    Move runtime shim to dedicated file

---
+++ runtime-shim.js
@@ -0,0 +1,39 @@
+module.exports = function ( nunjucks, env, obj, __require ) {
+
+	var oldRoot = obj.root;
+
+	obj.root = function( env, context, frame, runtime, cb ) {
+		var oldGetTemplate = env.getTemplate;
+		env.getTemplate = function( name, ec, parentName, cb ) {
+			if( typeof ec === "function" ) {
+				cb = ec;
+				ec = false;
+			}
+
+			var _require = function(name) {
+				try {
+					return __require(name);
+				} catch (e) {
+					if ( frame.get( "_require" ) ) return frame.get( "_require" )( name );
+				}
+			};
+			var tmpl = _require( name );
+			frame.set( "_require", _require );
+			if( ec ) tmpl.compile();
+			cb( null, tmpl );
+		};
+
+		oldRoot( env, context, frame, runtime, function( err, res ) {
+			env.getTemplate = oldGetTemplate;
+			cb( err, res );
+		} );
+	};
+
+	var src = {
+		obj: obj,
+		type: "code"
+	};
+
+	return new nunjucks.Template( src, env );
+
+};
